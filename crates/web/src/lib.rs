0ecbe68a7f24d74f7e1cc3e692645603660c9554
Thomas Reichert <thomasr1987@gmail.com>
2025-04-22 08:39:40 +1000

    WASM: Stop app function

--- crates/web/src/lib.rs
+++ crates/web/src/lib.rs
@@ -1,19 +1,44 @@
-use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, prelude::*};
 use shining_piano_core::ShiningPianoPlugin;
+use std::sync::atomic::{AtomicBool, Ordering};
 use wasm_bindgen::prelude::*;
+
+static EXIT_REQUESTED: AtomicBool = AtomicBool::new(false);
 
 #[wasm_bindgen]
 pub fn run_app() {
+    EXIT_REQUESTED.store(false, Ordering::SeqCst);
     App::new()
         .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    canvas: Some("#app-canvas".into()),
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        canvas: Some("#app-canvas".into()),
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(AssetPlugin {
+                    meta_check: AssetMetaCheck::Never,
                     ..default()
                 }),
-                ..default()
-            }),
             ShiningPianoPlugin,
         ))
+        .add_systems(Update, check_game_exit)
         .run();
 }
+
+#[wasm_bindgen]
+pub fn stop_app() {
+    EXIT_REQUESTED.store(true, Ordering::SeqCst);
+}
+
+fn check_game_exit(
+    mut exit: EventWriter<AppExit>
+) {
+    if EXIT_REQUESTED.load(Ordering::SeqCst) {
+        exit.send(AppExit::Success);
+    }
+}
+
